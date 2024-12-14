e97b50484db49b30948b87bd5c666abf66f5ed9e
clearxheaded <7777x777x7777@gmail.com>
2024-12-14 11:55:42 -0500

    Solve problem 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splittedString = string.split("");
+    let reverseArray = splittedString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
 };
 
 // Do not edit below this line
