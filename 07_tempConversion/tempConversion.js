9eff1acb56c9a3db6232835de4f2cb3d4bced9a1
clearxheaded <7777x777x7777@gmail.com>
2024-12-14 12:19:51 -0500

    Solve problems 05 06 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) * 5/9;
+  if (celsius === 0) return 0;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = (temp * 1.8) + 32;
+  if (fahrenheit === 0) return 0;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
